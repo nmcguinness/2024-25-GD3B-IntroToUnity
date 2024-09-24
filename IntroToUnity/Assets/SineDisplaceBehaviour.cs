0641f3f80f77c9532bbfcf3d9159aed6c55322b4
Niall McGuinness <niall.mcguinness@dkit.ie>
2024-09-24 10:05:48 +0100

    Update SineDisplaceBehaviour.cs

--- IntroToUnity/Assets/SineDisplaceBehaviour.cs
+++ IntroToUnity/Assets/SineDisplaceBehaviour.cs
@@ -6,12 +6,17 @@ public class SineDisplaceBehaviour : MonoBehaviour
     [Tooltip("Transform of the object to be displaced")]
     private Transform target;
 
-    // This function is called every fixed framerate frame, if the MonoBehaviour is enabled
-    private void FixedUpdate()
+    [SerializeField]
+    [Range(0f, 5f)]
+    private float amplitude = 1;
+
+    private Vector3 direction = Vector3.up;
+
+    //phase and angular speed
+
+    private void Update()
     {
-        //target.transform.Translate();
-        //Mathf.Sin()
-        //Time.time
-        //Amplitude, frequency, direction
+        var magnitude = amplitude * Mathf.Sin(Time.time);
+        target.transform.Translate(direction * magnitude);
     }
 }
