2d0a64ee104430c02d74c4a5aaeb1f14f9f40969
Niall McGuinness <niall.mcguinness@dkit.ie>
2024-10-01 08:59:27 +0100

    :truck: Added CS attribute for audio clip

--- IntroToUnity/Assets/GD/Common/Scripts/Selection/Providers/GameObjectRayProvider.cs
+++ IntroToUnity/Assets/GD/Common/Scripts/Selection/Providers/GameObjectRayProvider.cs
@@ -1,23 +1,27 @@
 using GD.Selection;
+using Unity.Android.Types;
 using UnityEngine;
 
 public class GameObjectRayProvider : MonoBehaviour, IRayProvider
 {
     [SerializeField]
+    [Tooltip("Normally set to the in-game player game object")]
     private GameObject rayOrigin;
+
+    [SerializeField]
+    [Tooltip("Set ray length")]
+    [Range(1, 100)]
+    private float rayLength = 20;
+
+    [SerializeField]
+    [Tooltip("Set ray color (non HDR)")]
+    private Color rayColor = Color.yellow;
 
     private Ray ray;
 
-    [SerializeField]
-    private Color rayColor = Color.yellow;
-
-    [SerializeField]
-    private float rayLength = 10;
-
     public Ray CreateRay()
     {
-        ray = new Ray(rayOrigin.transform.position,
-            rayOrigin.transform.forward);
+        ray = new Ray(rayOrigin.transform.position, rayOrigin.transform.forward);
         return ray;
     }
 
