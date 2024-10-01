dc5c9325af30869125161bdfd947d0491b029a0d
Niall McGuinness <niall.mcguinness@dkit.ie>
2024-10-01 11:11:29 +0100

    Fixed spherecast

--- IntroToUnity/Assets/GD/Common/Scripts/Selection/Selectors/SphereCastSelector.cs
+++ IntroToUnity/Assets/GD/Common/Scripts/Selection/Selectors/SphereCastSelector.cs
@@ -2,19 +2,24 @@ using UnityEngine;
 
 namespace GD.Selection
 {
+    //BUG - NMCG - Is this working properly?
     public class SphereCastSelector : MonoBehaviour, ISelector
     {
-        [SerializeField] private string selectableTag;
+        [SerializeField] private string selectableTag = "Selectable";
+        [SerializeField] private LayerMask layerMask;
         [SerializeField] private float radius = 1;
         [SerializeField] private int maxDistance = 10;
-        [SerializeField] private LayerMask layerMask = LayerMask.NameToLayer("Default");
 
         private Transform selection;
+        private Ray ray;
         private RaycastHit hitInfo;
 
         public void Check(Ray ray)
         {
             selection = null;
+            this.ray = ray;
+
+            //    RaycastHit[] hitInfoArray = Physics.SphereCastAll(ray, radius);
 
             if (Physics.SphereCast(ray, radius, out hitInfo, maxDistance, layerMask.value))
             {
@@ -22,6 +27,12 @@ namespace GD.Selection
                 if (currentSelection.CompareTag(selectableTag))
                     selection = currentSelection;
             }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawSphere(ray.origin, radius);
         }
 
         public RaycastHit GetHitInfo()
