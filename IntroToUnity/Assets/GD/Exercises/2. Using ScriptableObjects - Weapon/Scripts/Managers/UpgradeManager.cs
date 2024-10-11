01d87f297b8ccff5e7ce82d84f9eff65621377e5
Niall McGuinness <niall.mcguinness@dkit.ie>
2024-10-11 13:08:30 +0100

    :construction: Adding events in next example

--- IntroToUnity/Assets/GD/Exercises/2. Using ScriptableObjects - Weapon/Scripts/Managers/UpgradeManager.cs
+++ IntroToUnity/Assets/GD/Exercises/2. Using ScriptableObjects - Weapon/Scripts/Managers/UpgradeManager.cs
@@ -1,13 +1,35 @@
-using System.Collections;
+using GD;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UpgradeManager : MonoBehaviour
 {
     [SerializeField]
-    private List<IModifyWeapon> mortarUpgrades;
+    private SerializableDictionary upgradeDictionary;
+
+    [SerializeField]
+    [RequireInterface(typeof(IModifyWeapon))]
+    private List<ScriptableObject> upgrades;
+
+    private void Awake()
+    {
+        upgradeDictionary = new SerializableDictionary();
+    }
 
     //Listen for upgrade event (weapondata)
 
     //Apply upgrade to the weapon data
+    [ContextMenu("Apply Upgrade")]
+    public void ApplyUpgrade(WeaponData w)
+    {
+        //upgrade once
+        //get a random index in the range
+        var index = Random.Range(0, upgrades.Count);
+
+        //get the upgrade at the index
+        var upgrade = upgrades[index] as IModifyWeapon;
+
+        //apply the upgrade
+        upgrade.Apply(w);
+    }
 }
--- IntroToUnity/Assets/GD/Exercises/2. Using ScriptableObjects - Weapon/Scripts/Weapons/WeaponBehaviour.cs
+++ IntroToUnity/Assets/GD/Exercises/2. Using ScriptableObjects - Weapon/Scripts/Weapons/WeaponBehaviour.cs
@@ -12,32 +12,5 @@ public class WeaponBehaviour : MonoBehaviour
     [ReadOnly]
     private bool isUpgraded;
 
-    [SerializeField]
-    [RequireInterface(typeof(IModifyWeapon))]
-    private List<ScriptableObject> upgrades;
-
     #endregion Fields
-
-    [ContextMenu("Apply Upgrade")]
-    public void ApplyUpgrade()
-    {
-        //upgrade once
-        if (isUpgraded)
-        {
-            Debug.LogWarning("Weapon is already upgraded");
-            return;
-        }
-
-        //get a random index in the range
-        var index = Random.Range(0, upgrades.Count);
-
-        //get the upgrade at the index
-        var upgrade = upgrades[index] as IModifyWeapon;
-
-        //apply the upgrade
-        upgrade.Apply(weaponData);
-
-        //set the upgrade flag
-        isUpgraded = true;
-    }
 }
